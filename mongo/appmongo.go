24d44342c1df6d72910ec248e47cda9074d249d6
dx <dexin_work@126.com>
2019-08-13 11:23:45 +0800

    add appid

--- mongo/appmongo.go
+++ mongo/appmongo.go
@@ -1,1 +1,33 @@
 package mongo
+
+// app mongo data action
+func (m Mongodb) InsertAppEntity(data interface{}) error {
+	return nil
+}
+
+func (m Mongodb) UpdateAppEntity(data interface{}) error {
+	return nil
+}
+
+func (m Mongodb) RemoveAppEntity(appName string) error {
+	return nil
+}
+
+// task mongo data action
+func (m Mongodb)InsertTaskEntity(data interface{}) error {
+	return nil
+}
+
+func (m Mongodb)RemoveTaskEntity(taskName string) error {
+	return nil
+}
+
+func (m Mongodb)UpdateTaskStatus(taskName string, status int32) error {
+	return nil
+}
+
+func (m Mongodb)UpdateTaskEndTime(taskName string, endTime int64) error {
+	return nil
+}
+
+
